e28756983ae8d33f3a5557c6cbd43c9fdcd5d9cc
ilevine16 <isabellalevine@gmail.com>
2024-04-19 12:36:20 -0400

    generated difficult maze 2

--- src/main/sim/sim_environments.py
+++ src/main/sim/sim_environments.py
@@ -122,11 +122,56 @@ OFFICE_HOURS_MAZE1 = Environment([
     Wall(0,-3,1,-3),
     Wall(1,-1,1,-3),
     Wall(0,-1,1,-1)
+]).scale(WALL_LENGTH)
 
+DIFFICULT_ENVIRONEMENT_2 = Environment([
+    Wall(0,0,0,2),
+    Wall(0,2,5,2),
+    Wall(5,2,5,1),
+    Wall(5,1,8,1),
+    Wall(8,1,8,2),
+    Wall(9,2,9,-9),
+    Wall(0,-9,9,-9),
+    Wall(0,-1,1,-1),
+    Wall(0,0,2,0),
+    Wall(2,0,2,-1),
+    Wall(3,0,3,1),
+    Wall(3,1,4,1),
+    Wall(4,1,4,0),
+    Wall(4,0,3,0),
+    Wall(1,-1,1,-2),
+    Wall(0,-2,1,-2),
+    Wall(0,-4,2,-4),
+    Wall(2,-2,2,-4),
+    Wall(4,-1,8,-1),
+    Wall(5,-1,5,-2),
+    Wall(3,-3,6,-3),
+    Wall(4,-2,4,-3),
+    Wall(3,-4,6,-4),
+    Wall(3,-4,3,-6),
+    Wall(3,-6,2,-8),
+    Wall(0,-8,1,-8),
+    Wall(1,-8,1,-7),
+    Wall(1,-7,5,-7),
+    Wall(3,-7,3,-8),
+    Wall(5,-7,5,-5),
+    Wall(5,-5,6,-5),
+    Wall(6,-5,6,-4),
+    Wall(7,-2,7,-5),
+    Wall(7,-5,8,-5),
+    Wall(8,-5,8,-4),
+    Wall(8,-4,9,-4),
+    Wall(5,-9,5,-8),
+    Wall(5,-8,7,-8),
+    Wall(7,-6,7,-8),
+    Wall(7,-6,8,-6),
+    Wall(8,-6,8,-7),
+    Wall(8,-7,9,-7),
+    Wall(0,0,0,-9)
 ]).scale(WALL_LENGTH)
 
 
 """
 Environment to be used in the simulation.
 """
-SIM_ENVIRONMENT = OFFICE_HOURS_MAZE1
+SIM_ENVIRONMENT = DIFFICULT_ENVIRONEMENT_2
